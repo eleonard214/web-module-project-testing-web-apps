427cc33282ad8f5b14bc836d5943b0a3d8a446f6
Erica Leonard <elynn1979@live.com>
2021-05-13 21:14:15 -0400

    6 tests left

--- src/components/ContactForm.test.js
+++ src/components/ContactForm.test.js
@@ -5,15 +5,25 @@ import userEvent from '@testing-library/user-event';
 import ContactForm from './ContactForm';
 
 test('renders without errors', ()=>{
-    
+   render(<ContactForm />);
+   const errors = screen.queryAllByAltText(/^(.*>(error)[^$]*)$/i);
+   expect(errors.length).toEqual(0);
 });
 
 test('renders the contact form header', ()=> {
-    
+    render(<ContactForm/>);
+    const header =screen.queryByText(/Contact Form/i);
+    expect(header).toBeInTheDocument();
+    expect(header).toBeTruthy();
+    expect(header).toHaveTextContent("Contact Form");    
 });
 
 test('renders ONE error message if user enters less then 5 characters into firstname.', async () => {
-    
+    render(<ContactForm/>);
+    const username = screen.getByLabelText("First Name*");
+  userEvent.type(username, "Eric");
+  const errors = screen.queryAllByText(/^(.*?(error)[^$]*)$/i);
+  expect(errors.length).toEqual(1);
 });
 
 test('renders THREE error messages if user enters no values into any fields.', async () => {
